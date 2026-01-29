774bfad35c14c1b04bb56d08c3a2fbf1abf57219
mashayeakh <islammasayekh@gmail.com>
2026-01-30 05:23:33 +0600

    Implemented login form

--- src/app/(commonLayout)/login/page.tsx
+++ src/app/(commonLayout)/login/page.tsx
@@ -1,9 +1,12 @@
+import { LoginFormPage } from '@/components/modules/authentication/login-form'
 import React from 'react'
 
 export default function LoginPage() {
     return (
-        <div>
-            this is a login page.
+        <div className="min-h-screen flex items-center justify-center bg-gray-50 p-6">
+            <div className="max-w-md w-full">
+                <LoginFormPage />
+            </div>
         </div>
     )
 }
