7ca224317b11dd7a19d82757ba235100f7c17d27
mashayeakh <islammasayekh@gmail.com>
2026-02-06 17:28:04 +0600

    removed duplicte auth-client

--- src/lib/auth-clients.ts
+++ src/lib/auth-clients.ts
@@ -21,7 +21,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000",
     fetchOptions: {
         credentials: "include",
     },
